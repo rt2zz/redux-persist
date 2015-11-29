c549e24cc620bcf3b2915038911ba4038769b6c5
Zack <zack@root-two.com>
2015-11-29 13:37:15 -0800

    warn if no localStorage

--- src/defaults/asyncLocalStorage.js
+++ src/defaults/asyncLocalStorage.js
@@ -2,6 +2,17 @@
 
 var nextTick = process && process.nextTick ? process.nextTick : setImmediate
 
+const noLS = process && process.env && process.env.NODE_ENV === 'production'
+  ? () => { /*noop*/ return null }
+  : () => {
+    console.error('redux-persist asyncLocalStorage requires a global localStorage object. Either use a different storage backend or if this is a universal redux application you probably should conditionally persist like so: https://gist.github.com/rt2zz/ac9eb396793f95ff3c3b')
+    return null
+  }
+  
+var localStorage = typeof localStroage !== 'undefined'
+  ? localStorage
+  : { getItem: noLS, setItem: noLS, removeItem: noLS, getAllKeys: noLS }
+
 export default {
   getItem: function (key, cb) {
     try {
