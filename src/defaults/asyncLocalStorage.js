39dee2a1b5cf82dfd97003132ed2c606ba88ac3c
Zack <zack@root-two.com>
2016-01-28 12:31:55 -0800

    single localStorage factory (with support for type==='session')

--- src/defaults/asyncLocalStorage.js
+++ src/defaults/asyncLocalStorage.js
@@ -1,7 +1,7 @@
 const genericSetImmediate = typeof setImmediate === 'undefined' ? global.setImmediate : setImmediate
 const nextTick = process && process.nextTick ? process.nextTick : genericSetImmediate
 
-const noLS = process && process.env && process.env.NODE_ENV === 'production'
+const noStorage = process && process.env && process.env.NODE_ENV === 'production'
   ? () => { /* noop */ return null }
   : () => {
     console.error('redux-persist asyncLocalStorage requires a global localStorage object. Either use a different storage backend or if this is a universal redux application you probably should conditionally persist like so: https://gist.github.com/rt2zz/ac9eb396793f95ff3c3b')
@@ -11,57 +11,72 @@ const noLS = process && process.env && process.env.NODE_ENV === 'production'
 function hasLocalStorage () {
   try {
     return typeof window === 'object' && typeof window.localStorage !== 'undefined'
-  } catch (e) {
-    return false
-  }
+  } catch (e) { return false }
 }
 
-var localStorage = hasLocalStorage()
-  ? window.localStorage
-  : { getItem: noLS, setItem: noLS, removeItem: noLS, getAllKeys: noLS }
+function hasSessionStorage () {
+  try {
+    return typeof window === 'object' && typeof window.sessionStorage !== 'undefined'
+  } catch (e) { return false }
+}
 
-export default {
-  getItem: function (key, cb) {
-    try {
-      var s = localStorage.getItem(key)
-      nextTick(() => {
-        cb(null, s)
-      })
-    } catch (e) {
-      cb(e)
-    }
-  },
-  setItem: function (key, string, cb) {
-    try {
-      localStorage.setItem(key, string)
-      nextTick(() => {
-        cb(null)
-      })
-    } catch (e) {
-      cb(e)
-    }
-  },
-  removeItem: function (key, cb) {
-    try {
-      localStorage.removeItem(key)
-      nextTick(() => {
-        cb(null)
-      })
-    } catch (e) {
-      cb(e)
-    }
-  },
-  getAllKeys: function (cb) {
-    try {
-      var keys = []
-      for (var i = 0; i < localStorage.length; i++) {
-        keys.push(localStorage.key(i))
+function getStorage (type) {
+  if (type === 'local') {
+    return hasLocalStorage()
+      ? window.localStorage
+      : { getItem: noStorage, setItem: noStorage, removeItem: noStorage, getAllKeys: noStorage }
+  }
+  if (type === 'session') {
+    hasSessionStorage()
+      ? window.sessionStorage
+      : { getItem: noStorage, setItem: noStorage, removeItem: noStorage, getAllKeys: noStorage }
+  }
+}
+export default function (type) {
+  let storage = getStorage(type)
+  return {
+    getItem: function (key, cb) {
+      try {
+        var s = storage.getItem(key)
+        nextTick(() => {
+          cb(null, s)
+        })
+      } catch (e) {
+        cb(e)
       }
-      nextTick(() => {
-        cb(null, keys)
-      })
-    } catch (e) {
-      cb(e)
+    },
+    setItem: function (key, string, cb) {
+      try {
+        storage.setItem(key, string)
+        nextTick(() => {
+          cb(null)
+        })
+      } catch (e) {
+        cb(e)
+      }
+    },
+    removeItem: function (key, cb) {
+      try {
+        storage.removeItem(key)
+        nextTick(() => {
+          cb(null)
+        })
+      } catch (e) {
+        cb(e)
+      }
+    },
+    getAllKeys: function (cb) {
+      try {
+        var keys = []
+        for (var i = 0; i < storage.length; i++) {
+          keys.push(storage.key(i))
+        }
+        nextTick(() => {
+          cb(null, keys)
+        })
+      } catch (e) {
+        cb(e)
+      }
     }
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
+const createAsyncLocalStorage = require('./defaults/asyncLocalStorage')
+
 module.exports.persistStore = require('./persistStore')
 module.exports.autoRehydrate = require('./autoRehydrate')
 module.exports.getStoredState = require('./getStoredState')
 module.exports.storages = {
-  asyncLocalStorage: require('./defaults/asyncLocalStorage'),
-  asyncSessionStorage: require('./defaults/asyncSessionStorage')
+  asyncLocalStorage: createAsyncLocalStorage('local'),
+  asyncSessionStorage: createAsyncLocalStorage('session')
 }
