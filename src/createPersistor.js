4c85ddfc6fd8f92d5a06fe83bafd08c2420f8d14
Zack Story <zack@root-two.com>
2016-04-03 13:20:28 -0700

    [persistor] implement pause/resume

--- src/createPersistor.js
+++ src/createPersistor.js
@@ -1,11 +1,12 @@
-import forEach from 'lodash.foreach'
 import * as constants from './constants'
 import createAsyncLocalStorage from './defaults/asyncLocalStorage'
+import forEach from 'lodash.foreach'
 import stringify from 'json-stringify-safe'
 
 export default function createPersistor (store, config) {
   // defaults
   const serialize = config.serialize || defaultSerialize
+  const deserialize = config.deserialize || defaultDeserialize
   const blacklist = config.blacklist || []
   const whitelist = config.whitelist || false
   const transforms = config.transforms || []
@@ -16,11 +17,15 @@ export default function createPersistor (store, config) {
   if (storage.keys && !storage.getAllKeys) storage = {...storage, getAllKeys: storage.keys}
 
   // initialize stateful values
+  let lastState = store.getState()
+  let paused = false
+  let purgeMode = false
   let storesToProcess = []
   let timeIterator = null
-  let lastState = store.getState()
 
   store.subscribe(() => {
+    if (paused) return
+
     let state = store.getState()
     forEach(state, (subState, key) => {
       if (whitelistBlacklistCheck(key)) return
@@ -52,6 +57,52 @@ export default function createPersistor (store, config) {
     if (blacklist.indexOf(key) !== -1) return true
     return false
   }
+
+  function adhocRehydrate (serialized, cb) {
+    let state = null
+    try {
+      let data = deserialize(serialized)
+      state = transforms.reduceRight((interState, transformer) => {
+        return transformer.out(interState)
+      }, data)
+    } catch (err) {
+      if (process.env.NODE_ENV !== 'production') console.warn('Error rehydrating data:', serialized, err)
+    }
+
+    store.dispatch(rehydrateAction(state))
+    cb && cb(null, state)
+  }
+
+  function purge (keys) {
+    purgeMode = keys
+    forEach(keys, (key) => {
+      storage.removeItem(createStorageKey(key), warnIfRemoveError(key))
+    })
+  }
+
+  function purgeAll () {
+    purgeMode = '*'
+    storage.getAllKeys((err, allKeys) => {
+      if (err && process.env.NODE_ENV !== 'production') { console.warn('Error in storage.getAllKeys') }
+      purge(allKeys.filter((key) => key.indexOf(constants.keyPrefix) === 0).map((key) => key.slice(constants.keyPrefix.length)))
+    })
+  }
+
+  // return `persistor`
+  return {
+    rehydrate: adhocRehydrate,
+    pause: () => { paused = true },
+    resume: () => { paused = false },
+    purge,
+    purgeAll,
+    _getPurgeMode: () => purgeMode
+  }
+}
+
+function warnIfRemoveError (key) {
+  return function removeError (err) {
+    if (err && process.env.NODE_ENV !== 'production') { console.warn('Error storing data for key:', key, err) }
+  }
 }
 
 function warnIfSetError (key) {
@@ -74,4 +125,15 @@ function defaultSerialize (data) {
       Cycle encounted at key "${k}" with value "${v}".
     `)
   })
+}
+
+function defaultDeserialize (serial) {
+  return JSON.parse(serial)
+}
+
+function rehydrateAction (data) {
+  return {
+    type: constants.REHYDRATE,
+    payload: data
+  }
 }
--- src/persistStore.js
+++ src/persistStore.js
@@ -1,6 +1,4 @@
-import forEach from 'lodash.foreach'
 import * as constants from './constants'
-import createAsyncLocalStorage from './defaults/asyncLocalStorage'
 import getStoredState from './getStoredState'
 import createPersistor from './createPersistor'
 
@@ -8,79 +6,29 @@ const genericSetImmediate = typeof setImmediate === 'undefined' ? global.setImme
 
 export default function persistStore (store, config = {}, onComplete) {
   // defaults
-  const deserialize = config.deserialize || defaultDeserialize
-  let storage = config.storage || createAsyncLocalStorage('local')
   const shouldRestore = !config.skipRestore
-  const transforms = config.transforms || []
 
-  // fallback getAllKeys to `keys` if present (LocalForage compatability)
-  if (storage.keys && !storage.getAllKeys) storage = {...storage, getAllKeys: storage.keys}
-
-  // purge: stateful variable triggered via chained method for convienence
-  let purgeMode = false
+  // create and pause persistor
+  const persistor = createPersistor(store, config)
+  persistor.pause()
 
   // restore
   if (shouldRestore) {
     genericSetImmediate(() => {
-      getStoredState({...config, purgeMode}, (err, restoredState) => {
-        if (err && process.env.NODE_ENV !== 'production') console.warn('Error in getStoredState', err)
-        store.dispatch(rehydrateAction(restoredState))
-        complete(null, restoredState)
+      getStoredState({...config, purgeMode: persistor._getPurgeMode()}, (err, restoredState) => {
+        if (restoredState) store.dispatch(rehydrateAction(restoredState))
+        if (err) store.dispatch(rehydrateErrorAction(err))
+        complete(err, restoredState)
       })
     })
   } else genericSetImmediate(complete)
 
   function complete (err, restoredState) {
-    createPersistor(store, config)
+    persistor.resume()
     onComplete && onComplete(err, restoredState)
   }
 
-  function adhocRehydrate (serialized, cb) {
-    let state = null
-    try {
-      let data = deserialize(serialized)
-      state = transforms.reduceRight((interState, transformer) => {
-        return transformer.out(interState)
-      }, data)
-    } catch (err) {
-      if (process.env.NODE_ENV !== 'production') console.warn('Error rehydrating data:', serialized, err)
-    }
-
-    store.dispatch(rehydrateAction(state))
-    cb && cb(null, state)
-  }
-
-  function purge (keys) {
-    purgeMode = keys
-    forEach(keys, (key) => {
-      storage.removeItem(createStorageKey(key), warnIfRemoveError(key))
-    })
-  }
-
-  function purgeAll () {
-    purgeMode = '*'
-    storage.getAllKeys((err, allKeys) => {
-      if (err && process.env.NODE_ENV !== 'production') { console.warn('Error in storage.getAllKeys') }
-      purge(allKeys.filter((key) => key.indexOf(constants.keyPrefix) === 0).map((key) => key.slice(constants.keyPrefix.length)))
-    })
-  }
-
-  // return `persistor`
-  return {
-    rehydrate: adhocRehydrate,
-    purge,
-    purgeAll
-  }
-}
-
-function warnIfRemoveError (key) {
-  return function removeError (err) {
-    if (err && process.env.NODE_ENV !== 'production') { console.warn('Error storing data for key:', key, err) }
-  }
-}
-
-function createStorageKey (key) {
-  return constants.keyPrefix + key
+  return persistor
 }
 
 function rehydrateAction (data) {
@@ -90,6 +38,9 @@ function rehydrateAction (data) {
   }
 }
 
-function defaultDeserialize (serial) {
-  return JSON.parse(serial)
+function rehydrateErrorAction (err) {
+  return {
+    type: constants.REHYDRATE_ERROR,
+    payload: err
+  }
 }
