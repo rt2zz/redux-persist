d8f10f6d64505b23388654799ec7dcc9fd6eb767
Zack Story <ztstory@gmail.com>
2016-04-26 17:46:21 -0700

    [tests] switch to ava

--- test/autoRehydrate.spec.js
+++ test/autoRehydrate.spec.js
@@ -1,8 +1,6 @@
-/* global it, describe */
+import test from 'ava'
 
 import { compose, createStore } from 'redux'
-import assert from 'assert'
-import { isEqual } from 'lodash'
 import Immutable from 'immutable'
 
 import { REHYDRATE } from '../src/constants'
@@ -19,29 +17,26 @@ const createReducer = (actionCallback) => {
 
 var finalCreateStore = compose(autoRehydrate())(createStore)
 
-describe('rehydrate actions', function () {
-  it('with array payload should overwrite substate', function () {
-    let store = finalCreateStore(createReducer())
-    store.dispatch(rehydrate({arraySpace: [1, 2]}))
-    let state = store.getState()
-    assert(isEqual(state, {arraySpace: [1, 2]}))
-  })
-  it('rehydrates string state', function () {
-    let store = finalCreateStore(createReducer())
-    store.dispatch(rehydrate({stringSpace: someString}))
-    let state = store.getState()
-    assert(isEqual(state.stringSpace, someString))
-  })
+test('rehydrate: with array payload should overwrite substate', (t) => {
+  let store = finalCreateStore(createReducer())
+  store.dispatch(rehydrate({arraySpace: [1, 2]}))
+  let state = store.getState()
+  t.deepEqual(state, {arraySpace: [1, 2]})
+})
+
+test('rehydrate: rehydrates string state', (t) => {
+  let store = finalCreateStore(createReducer())
+  store.dispatch(rehydrate({stringSpace: someString}))
+  let state = store.getState()
+  t.deepEqual(state.stringSpace, someString)
 })
 
 const rehydrate = (payload) => ({type: REHYDRATE, payload})
 
-describe('when rehydrating immutable data', () => {
-  it('does not warn', () => {
-    let store = finalCreateStore(createReducer())
-    let immutableData = Immutable.Map({a: 1})
-    store.dispatch(rehydrate({immutableSpace: immutableData}))
-    let state = store.getState()
-    assert(state.immutableSpace.equals(immutableData))
-  })
+test('can rehydrating immutable state', (t) => {
+  let store = finalCreateStore(createReducer())
+  let immutableData = Immutable.Map({a: 1})
+  store.dispatch(rehydrate({immutableSpace: immutableData}))
+  let state = store.getState()
+  t.truthy(state.immutableSpace.equals(immutableData))
 })
--- test/persistStore.spec.js
+++ test/persistStore.spec.js
@@ -1,10 +1,9 @@
-/* global it, describe */
+import test from 'ava'
 
 import { persistStore, getStoredState } from '../src'
 import * as constants from '../src/constants'
-import createMemoryStorage from './mock/createMemoryStorage'
-import assert from 'assert'
-import { isEqual } from 'lodash'
+import isEqual from 'lodash/isEqual'
+import createMemoryStorage from './helpers/createMemoryStorage'
 
 function createMockStore (opts) {
   return {
@@ -16,66 +15,75 @@ function createMockStore (opts) {
   }
 }
 
-describe('persistStore scenarios', function () {
-  it('Returns state to persistStore callback', function (done) {
-    let store = createMockStore({
-      mockState: {foo: 1, bar: 2},
-      dispatch: () => {}
-    })
+test.cb('Returns state to persistStore callback', (t) => {
+  let store = createMockStore({
+    mockState: {foo: 1, bar: 2},
+    dispatch: () => {}
+  })
 
-    let seedState = {foo: 3, bar: 4}
-    persistStore(store, { storage: createMemoryStorage(seedState) }, (err, state) => {
-      if (err) throw new Error()
-      assert(isEqual(state, seedState))
-      done()
-    })
+  let seedState = {foo: 3, bar: 4}
+  persistStore(store, { storage: createMemoryStorage(seedState) }, (err, state) => {
+    t.ifError(err, 'persistStore errored')
+    t.deepEqual(state, seedState)
+    t.pass()
+    t.end()
   })
 })
 
-describe('getStoredState', function () {
-  it('returns stored state', function (done) {
-    let seedState = {foo: 3, bar: 4}
-    getStoredState({ storage: createMemoryStorage(seedState) }, (err, state) => {
-      if (err) throw new Error()
-      assert(isEqual(state, seedState))
-      done()
-    })
+test.cb('returns stored state', (t) => {
+  let seedState = {foo: 3, bar: 4}
+  getStoredState({ storage: createMemoryStorage(seedState) }, (err, state) => {
+    t.ifError(err, 'persistStore errored')
+    t.deepEqual(state, seedState)
+    t.pass()
+    t.end()
   })
 })
 
-describe('adhoc rehydrate', function () {
-  it('processes adhoc rehydrate with serial: true', function (done) {
-    let store = createMockStore({
-      mockState: { foo: 3, bar: [1] },
-      dispatch: (action) => {
-        if (action.type === constants.REHYDRATE) {
-          if (isEqual(action.payload, {bar: {a: 'b'}})) {
-            done()
-          }
+test.cb('processes adhoc rehydrate with serial: true', (t) => {
+  let store = createMockStore({
+    mockState: { foo: 3, bar: [1] },
+    dispatch: (action) => {
+      if (action.type === constants.REHYDRATE) {
+        if (isEqual(action.payload, {bar: {a: 'b'}})) {
+          t.pass()
+          t.end()
         }
       }
-    })
-    let seedState = {foo: 3, bar: 4}
-    const persistor = persistStore(store, { storage: createMemoryStorage(seedState) }, (err, state) => {
-      if (err) throw new Error()
-      persistor.rehydrate({bar: JSON.stringify({a: 'b'})}, {serial: true})
-    })
+    }
   })
-  it('processes adhoc rehydrate with serial: false', function (done) {
-    let store = createMockStore({
-      mockState: { foo: 3, bar: [1] },
-      dispatch: (action) => {
-        if (action.type === constants.REHYDRATE) {
-          if (isEqual(action.payload, {bar: {a: 'b'}})) {
-            done()
-          }
+
+  let seedState = {foo: 3, bar: 4}
+  const persistor = persistStore(store, { storage: createMemoryStorage(seedState) }, (err, state) => {
+    if (err) throw new Error()
+    persistor.rehydrate({bar: JSON.stringify({a: 'b'})}, {serial: true})
+  })
+
+  setTimeout(() => {
+    throw new Error('timeout')
+  }, 10000)
+})
+
+test.cb('processes adhoc rehydrate with serial: false', (t) => {
+  let store = createMockStore({
+    mockState: { foo: 3, bar: [1] },
+    dispatch: (action) => {
+      if (action.type === constants.REHYDRATE) {
+        if (isEqual(action.payload, {bar: {a: 'b'}})) {
+          t.pass()
+          t.end()
         }
       }
-    })
-    let seedState = {foo: 3, bar: 4}
-    const persistor = persistStore(store, { storage: createMemoryStorage(seedState) }, (err, state) => {
-      if (err) throw new Error()
-      persistor.rehydrate({bar: {a: 'b'}})
-    })
+    }
   })
+
+  let seedState = {foo: 3, bar: 4}
+  const persistor = persistStore(store, { storage: createMemoryStorage(seedState) }, (err, state) => {
+    if (err) throw new Error()
+    persistor.rehydrate({bar: {a: 'b'}})
+  })
+
+  setTimeout(() => {
+    throw new Error('timeout')
+  }, 10000)
 })
