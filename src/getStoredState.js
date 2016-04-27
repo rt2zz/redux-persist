a538ca3f68392d96f87762d129731aa9d43bcfda
Zack Story <ztstory@gmail.com>
2016-04-27 11:55:57 -0700

    [getStoredState] add preloader support

--- src/getStoredState.js
+++ src/getStoredState.js
@@ -7,6 +7,7 @@ export default function getStoredState (config, onComplete) {
   const deserialize = config.deserialize || defaultDeserialize
   const transforms = config.transforms || []
   const purgeMode = config.purgeMode || false
+  const preloaders = config.preloaders || []
 
   // fallback getAllKeys to `keys` if present (LocalForage compatability)
   if (storage.keys && !storage.getAllKeys) storage = {...storage, getAllKeys: storage.keys}
@@ -14,21 +15,24 @@ export default function getStoredState (config, onComplete) {
   let restoredState = {}
   let completionCount = 0
 
-  storage.getAllKeys((err, allKeys) => {
-    if (err && process.env.NODE_ENV !== 'production') { console.warn('Error in storage.getAllKeys') }
-    let persistKeys = allKeys.filter((key) => key.indexOf(constants.keyPrefix) === 0).map((key) => key.slice(constants.keyPrefix.length))
-    let keysToRestore = Array.isArray(purgeMode)
-      ? persistKeys.filter((key) => purgeMode.indexOf(key) === -1)
-      : purgeMode === '*' ? [] : persistKeys
+  // @TODO skip promises is no preloaders
+  Promise.all(preloaders.map(p => p(config))).then(() => {
+    storage.getAllKeys((err, allKeys) => {
+      if (err && process.env.NODE_ENV !== 'production') { console.warn('Error in storage.getAllKeys') }
+      let persistKeys = allKeys.filter((key) => key.indexOf(constants.keyPrefix) === 0).map((key) => key.slice(constants.keyPrefix.length))
+      let keysToRestore = Array.isArray(purgeMode)
+        ? persistKeys.filter((key) => purgeMode.indexOf(key) === -1)
+        : purgeMode === '*' ? [] : persistKeys
 
-    let restoreCount = keysToRestore.length
-    if (restoreCount === 0) complete(null, restoredState)
-    forEach(keysToRestore, (key) => {
-      storage.getItem(createStorageKey(key), (err, serialized) => {
-        if (err && process.env.NODE_ENV !== 'production') console.warn('Error restoring data for key:', key, err)
-        else restoredState[key] = rehydrate(key, serialized)
-        completionCount += 1
-        if (completionCount === restoreCount) complete(null, restoredState)
+      let restoreCount = keysToRestore.length
+      if (restoreCount === 0) complete(null, restoredState)
+      forEach(keysToRestore, (key) => {
+        storage.getItem(createStorageKey(key), (err, serialized) => {
+          if (err && process.env.NODE_ENV !== 'production') console.warn('Error restoring data for key:', key, err)
+          else restoredState[key] = rehydrate(key, serialized)
+          completionCount += 1
+          if (completionCount === restoreCount) complete(null, restoredState)
+        })
       })
     })
   })
