ff96060983a830a5f54c8cad2da6741f41a1bf7f
Kevin Donahue <kevindonahuedevelopment@gmail.com>
2016-11-22 16:25:58 -0500

    Cleanup createTransform inbound test

--- test/createTransform.spec.js
+++ test/createTransform.spec.js
@@ -6,9 +6,8 @@ import { createTransform, getStoredState, persistStore } from '../src'
 import createMemoryStorage from './helpers/createMemoryStorage'
 const ACTION_MUTATE_ALL = 'ACTION_MUTATE_ALL'
 
-const createReducer = (actionCallback) => {
+const createReducer = () => {
   return (state = {a: 1, b: 2}, action) => {
-    actionCallback && actionCallback(action)
     if (action.type === ACTION_MUTATE_ALL) {
       return {a: 10, b: 20}
     }
@@ -27,24 +26,26 @@ test('createTransform outbound', async function (t) {
   t.deepEqual(state, {a: 'hi', b: 'hi'})
 })
 
-// a terribly ugly test for inbound transforms @TODO rewrite
 test.cb('createTransform inbound', (t) => {
   const store = createStore(createReducer())
   const seedState = {a: 1, b: {b0: 2}}
 
   const inbound = (state, key) => key
   const outbound = (state, key) => 'hi'
-  let testTransform = createTransform(inbound, outbound)
-  let memoryStorage = createMemoryStorage(seedState)
-  persistStore(store, {storage: memoryStorage, transforms: [testTransform]})
 
-  setTimeout(() => {
+  const memoryStorage = createMemoryStorage(seedState)
+  const testTransform = createTransform(inbound, outbound)
+  const config = {storage: memoryStorage, transforms: [testTransform]}
+  persistStore(store, config, (err, restoredState) => {
+    t.ifError(err)
+    // t.deepEqual(restoredState, {a: 'hi', b: 'hi'})
+
     store.dispatch({type: ACTION_MUTATE_ALL})
-    setTimeout(async function () {
-      let state = await getStoredState({storage: memoryStorage})
+
+    setImmediate(async () => {
+      const state = await getStoredState({storage: memoryStorage})
       t.deepEqual(state, {a: 'a', b: 'b'})
-      t.pass()
       t.end()
-    }, 20)
-  }, 50)
+    })
+  })
 })
