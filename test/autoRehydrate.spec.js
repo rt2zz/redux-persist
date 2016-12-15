dcfcecc027590abfaa594a945eb0a6060cc4515e
rt2zz <zack@root-two.com>
2016-12-14 18:33:05 -0800

    add initialState test

--- test/autoRehydrate.spec.js
+++ test/autoRehydrate.spec.js
@@ -1,6 +1,6 @@
 import test from 'ava'
 
-import { compose, createStore } from 'redux'
+import { createStore } from 'redux'
 import Immutable from 'immutable'
 
 import { REHYDRATE } from '../src/constants'
@@ -15,17 +15,24 @@ const createReducer = (actionCallback) => {
   }
 }
 
-var finalCreateStore = compose(autoRehydrate())(createStore)
+var configureStore = (reducer, initState) => createStore(reducer, initState, autoRehydrate())
 
 test('rehydrate: with array payload should overwrite substate', (t) => {
-  let store = finalCreateStore(createReducer())
+  let store = configureStore(createReducer())
   store.dispatch(rehydrate({arraySpace: [1, 2]}))
   let state = store.getState()
   t.deepEqual(state.arraySpace, [1, 2])
 })
 
+test('rehydrate: should work with initState', (t) => {
+  let store = configureStore(createReducer(), {a: 1})
+  store.dispatch(rehydrate({a: 2}))
+  let state = store.getState()
+  t.deepEqual(state.a, 2)
+})
+
 test('rehydrate: rehydrates string state', (t) => {
-  let store = finalCreateStore(createReducer())
+  let store = configureStore(createReducer())
   store.dispatch(rehydrate({stringSpace: someString}))
   let state = store.getState()
   t.deepEqual(state.stringSpace, someString)
@@ -34,7 +41,7 @@ test('rehydrate: rehydrates string state', (t) => {
 const rehydrate = (payload) => ({type: REHYDRATE, payload})
 
 test('can rehydrating immutable state', (t) => {
-  let store = finalCreateStore(createReducer())
+  let store = configureStore(createReducer())
   let immutableData = Immutable.Map({a: 1})
   store.dispatch(rehydrate({immutableSpace: immutableData}))
   let state = store.getState()
@@ -42,7 +49,7 @@ test('can rehydrating immutable state', (t) => {
 })
 
 test('does not rehydrate unknown state keys', (t) => {
-  let store = finalCreateStore(createReducer())
+  let store = configureStore(createReducer())
   let someData = 1
   store.dispatch(rehydrate({someData}))
   let state = store.getState()
