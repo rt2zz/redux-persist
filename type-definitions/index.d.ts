98bf29314a4244e997449e3d35975219827f9b6d
Marvin Hagemeister <marvin@marvinhagemeister.de>
2017-02-10 20:08:58 +0100

    Cleanup TypeScript declaration file
    - Don't export internal methods
    - Fix typing issue in combination with localforage
    
    Fixes #273.

--- type-definitions/index.d.ts
+++ type-definitions/index.d.ts
@@ -1,14 +1,12 @@
-declare module "redux-persist/lib/interfaces" {
-  export {
-    Store,
-    StoreEnhancer,
-  } from "redux";
+declare module "redux-persist" {
+  import { Store, StoreEnhancer } from "redux";
 
   export interface Storage {
     setItem: Function;
     getItem: Function;
     removeItem: Function;
-    getAllKeys: Function;
+    getAllKeys?: Function;
+    keys?: Function;
   }
 
   export interface PersistorConfig {
@@ -37,7 +35,7 @@ declare module "redux-persist/lib/interfaces" {
     (err?: any, result?: Object): void;
   }
 
-  export  interface Persistor {
+  export interface Persistor {
     purge: (keys?: string[]) => void;
     rehydrate: (incoming: Object, options: { serial: boolean }) => void;
     pause: () => void;
@@ -52,99 +50,36 @@ declare module "redux-persist/lib/interfaces" {
     log?: boolean;
     stateReconciler?: StateReconciler<any, any>;
   }
-}
 
-declare module "redux-persist/lib/autoRehydrate" {
-  import {
-    StoreEnhancer,
-    AutoRehydrateConfig,
-  } from "redux-persist/lib/interfaces";
+  export function autoRehydrate<State>(autoRehydrateConfig?: AutoRehydrateConfig): StoreEnhancer<State>;
 
-  function autoRehydrate<State>(autoRehydrateConfig?: AutoRehydrateConfig): StoreEnhancer<State>;
-
-  export default autoRehydrate;
-}
-
-declare module "redux-persist/lib/createPersistor" {
-  import {
-    Store,
-    PersistorConfig,
-    Persistor,
-  } from "redux-persist/lib/interfaces";
-
-  function createPersistor<State>(store: Store<State>, persistorConfig: PersistorConfig): Persistor;
-
-  export default createPersistor;
-}
-
-declare module "redux-persist/lib/createTransform" {
-  import {
-    TransformIn,
-    TransformOut,
-    Transform,
-  } from "redux-persist/lib/interfaces";
+  export function createPersistor<State>(store: Store<State>, persistorConfig: PersistorConfig): Persistor;
 
   export interface TransformConfig {
     whitelist?: string[];
     blacklist?: string[];
   }
 
-  function createTransform<State, Raw>(transformIn: TransformIn<State, Raw>, transformOut: TransformOut<Raw, State>, config?: TransformConfig): Transform<State, Raw>;
+  export function createTransform<State, Raw>(transformIn: TransformIn<State, Raw>, transformOut: TransformOut<Raw, State>, config?: TransformConfig): Transform<State, Raw>;
 
-  export default createTransform;
-}
+  export function getStoredState(persistorConfig?: PersistorConfig, onComplete?: OnComplete): void;
 
-declare module "redux-persist/lib/getStoredState" {
-  import {
-    OnComplete,
-    PersistorConfig,
-  } from "redux-persist/lib/interfaces";
+  export function persistStore<State>(store: Store<State>, persistorConfig?: PersistorConfig, onComplete?: OnComplete): Persistor;
 
-  function getStoredState(persistorConfig?: PersistorConfig, onComplete?: OnComplete): void;
-
-  export default getStoredState;
-}
-
-declare module "redux-persist/lib/persistStore" {
-  import {
-    OnComplete,
-    Store,
-    PersistorConfig,
-    Persistor,
-  } from "redux-persist/lib/interfaces";
-
-  function persistStore<State>(store: Store<State>, persistorConfig?: PersistorConfig, onComplete?: OnComplete): Persistor;
-
-  export default persistStore;
-}
-
-declare module "redux-persist/lib/purgeStoredState" {
-  import {
-    PersistorConfig,
-  } from "redux-persist/lib/interfaces";
-
-  function purgeStoredState(persistorConfig?: PersistorConfig, keys?: string[]): Promise<void>;
-
-  export default purgeStoredState;
-}
-
-declare module "redux-persist/constants" {
-  export const KEY_PREFIX = "reduxPersist:"
-  export const REHYDRATE = "persist/REHYDRATE"
-}
-
-declare module "redux-persist" {
-  export { Storage } from "redux-persist/lib/interfaces";
-
-  export { default as autoRehydrate } from "redux-persist/lib/autoRehydrate";
-  export { default as createPersistor } from "redux-persist/lib/createPersistor";
-  export { default as createTransform } from "redux-persist/lib/createTransform";
-  export { default as getStoredState } from "redux-persist/lib/getStoredState";
-  export { default as persistStore } from "redux-persist/lib/persistStore";
-  export { default as purgeStoredState } from "redux-persist/lib/purgeStoredState";
+  export function purgeStoredState(persistorConfig?: PersistorConfig, keys?: string[]): Promise<void>;
 
   export const storages: {
     asyncLocalStorage: Storage,
     asyncSessionStorage: Storage,
   };
+}
+
+declare module "redux-persist/constants" {
+  export const KEY_PREFIX;
+  export const REHYDRATE;
+}
+
+declare module "redux-persist/storages" {
+  export const asyncLocalStorage: Storage;
+  export const asyncSessionStorage: Storage;
 }
