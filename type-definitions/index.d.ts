7148bab333dacebeef4b1b6006b268282dc7cbd1
Karol Janyst <lapkom@gmail.com>
2017-02-21 07:12:23 +0900

    (types): Fix missing noop, change some types to preferred ones (void -> undefined) (#282)
    * Fix bug with missing type, copy definitions to es6 version
    
    * Fix some ts types

--- type-definitions/index.d.ts
+++ type-definitions/index.d.ts
@@ -2,11 +2,11 @@ declare module "redux-persist" {
   import { Store, StoreEnhancer } from "redux";
 
   export interface Storage {
-    setItem(key: string, value: any, onComplete?: OnComplete<void>): Promise<void>;
+    setItem(key: string, value: any, onComplete?: OnComplete<any>): Promise<any>;
     getItem<Result>(key: string, onComplete?: OnComplete<Result>): Promise<Result>;
-    removeItem(key: string, onComplete?: OnComplete<void>): Promise<void>;
+    removeItem(key: string, onComplete?: OnComplete<any>): Promise<any>;
     getAllKeys<Result>(onComplete?: OnComplete<Result>): Promise<Result>;
-    keys?: Noop;
+    keys?: (...args: any[]) => any;
     [key: string]: any; // In case Storage object has some other (private?) methods and properties.
   }
 
@@ -29,7 +29,7 @@ declare module "redux-persist" {
     out: TransformOut<Raw, State>;
   }
 
-  export type OnComplete<Result> = (err?: any, result?: Result) => void;
+  export type OnComplete<Result> = (err?: any, result?: Result) => any;
 
   export interface RehydrateOptions {
     serial?: boolean;
@@ -37,7 +37,7 @@ declare module "redux-persist" {
 
   export interface Persistor {
     purge(keys?: string[]): void;
-    rehydrate<State>(incoming: State, options: RehydrateOptions): void;
+    rehydrate<State>(incoming: State, options: RehydrateOptions): undefined;
     pause(): void;
     resume(): void;
   }
@@ -60,11 +60,11 @@ declare module "redux-persist" {
 
   export function createTransform<State, Raw>(transformIn: TransformIn<State, Raw>, transformOut: TransformOut<Raw, State>, config?: TransformConfig): Transform<State, Raw>;
 
-  export function getStoredState(persistorConfig?: PersistorConfig, onComplete?: OnComplete<any>): void;
+  export function getStoredState(persistorConfig?: PersistorConfig, onComplete?: OnComplete<any>): undefined;
 
   export function persistStore<State>(store: Store<State>, persistorConfig?: PersistorConfig, onComplete?: OnComplete<any>): Persistor;
 
-  export function purgeStoredState(persistorConfig?: PersistorConfig, keys?: string[]): Promise<void>;
+  export function purgeStoredState(persistorConfig?: PersistorConfig, keys?: string[]): Promise<any>;
 
   import * as storages from "redux-persist/storages";
   export { storages };
