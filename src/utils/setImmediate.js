b5528bec7f3d02ee49ef123da8216bc2d6aebd9b
Sergey Pchelintsev <linz.sergey@gmail.com>
2017-06-29 15:06:15 +0300

    Fix IE support

--- src/utils/setImmediate.js
+++ src/utils/setImmediate.js
@@ -1,3 +1,4 @@
-const setImmediate = typeof global !== 'undefined' && typeof global.setImmediate !== 'undefined' ? global.setImmediate : (fn, ms) => setTimeout(fn, ms)
+const hasNativeSupport = typeof global !== 'undefined' && typeof global.setImmediate !== 'undefined'
+const setImmediate = hasNativeSupport ? (fn, ms) => global.setImmediate(fn, ms) : (fn, ms) => setTimeout(fn, ms)
 
 export default setImmediate
