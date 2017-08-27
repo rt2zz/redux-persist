32462e3449b3dda27119242795550e768ad43ec5
Noitidart <noitidart@gmail.com>
2017-08-27 07:06:02 -0700

    support async outbound transforms

--- src/getStoredState.js
+++ src/getStoredState.js
@@ -24,13 +24,20 @@ export default function getStoredState(config: PersistConfig): Promise<Object> {
       else {
         try {
           let state = {}
-          let rawState = deserialize(serialized)
-          Object.keys(rawState).forEach(key => {
-            state[key] = transforms.reduceRight((subState, transformer) => {
-              return transformer.out(subState, key)
-            }, deserialize(rawState[key]))
-          })
-          resolve(state)
+          let rawState = deserialize(serialized);
+          // run transforms on each state key in paralell, run transforms within a key in serial
+          (async function() {
+            const keyTransforms = Object.keys(rawState).map( async key => {
+              state[key] = deserialize(rawState[key]);
+              const transformsCnt = transforms.length - 1;
+              for (let i=transformsCnt; i>=0; i--) {
+                const transformer = transforms[i];
+                state[key] = await transformer.out(state[key], key);
+              }
+            })
+            await Promise.all(keyTransforms);
+            resolve(state);
+          })()
         } catch (err) {
           if (process.env.NODE_ENV !== 'production' && debug)
             console.log(
