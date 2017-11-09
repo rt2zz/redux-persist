5e4051586d407841027e331fb8ba759e4bae01ca
rt2zz <zack@root-two.com>
2017-11-08 16:10:13 -0800

    (module): export constants from the main module

--- src/index.js
+++ src/index.js
@@ -8,3 +8,5 @@ export { default as createTransform } from './createTransform'
 export { default as getStoredState } from './getStoredState'
 export { default as createPersistoid } from './createPersistoid'
 export { default as purgeStoredState } from './purgeStoredState'
+
+export * from './constants'
