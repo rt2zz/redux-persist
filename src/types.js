adaba5c6ecc05426a95e02074635c03fd6a9d343
rt2zz <zack@root-two.com>
2017-11-13 10:08:32 -0800

    (types): update flow-bin, fix transformer type

--- src/types.js
+++ src/types.js
@@ -39,8 +39,8 @@ export type MigrationManifest = {
 }
 
 export type Transform = {
-  inbound: (Object | string, string) => Object,
-  outbound: (Object | string, string) => Object,
+  in: (Object | string, string) => Object,
+  out: (Object | string, string) => Object,
   config?: PersistConfig,
 }
 
