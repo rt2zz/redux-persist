f211a0bf1206905ddc346818321c6e1326d1272e
rt2zz <zack@root-two.com>
2017-11-14 12:12:22 -0800

    (stateReconciler): remove not too useful dev checks

--- src/stateReconciler/autoMergeLevel2.js
+++ src/stateReconciler/autoMergeLevel2.js
@@ -15,50 +15,6 @@ export default function autoMergeLevel2<State: Object>(
   reducedState: State,
   { debug }: PersistConfig
 ): State {
-  // various dev only sanity checks
-  if (process.env.NODE_ENV !== 'production') {
-    if (inboundState) {
-      Object.keys(inboundState).forEach(key => {
-        // check if initialState is missing a key
-        if (!originalState.hasOwnProperty(key))
-          console.log(
-            `
-          redux-persist/stateReconciler: state missing key
-          "${key}". state-manager will still store the rehydrated value. If you
-          removed ${key} from your reducer tree, you should write a migration to
-          remove ${key} from stored state. If you code-split ${key} reducer, then
-          this is the expected behavior.
-        `
-          )
-
-        // check recently added reducer properties that may require a migration
-        if (
-          originalState[key] &&
-          typeof originalState[key] === 'object' &&
-          inboundState[key] &&
-          typeof inboundState[key] === 'object'
-        ) {
-          const stateKeys = originalState[key]
-            ? Object.keys(originalState[key])
-            : []
-          const inboundStateKeys = Object.keys(inboundState[key])
-          stateKeys.forEach(checkKey => {
-            if (inboundState[checkKey] === 'undefined')
-              console.log(
-                `
-              redux-persist/stateReconciler: initialState for "${key}"
-              has property "${checkKey}" which is missing in rehydratedState. After
-              rehydration, "${checkKey}" will be null. If you recently added
-              ${checkKey} to your ${key} reducer, consider adding ${checkKey} to a
-              state migration.
-            `
-              )
-          })
-        }
-      })
-    }
-  }
-
   let newState = { ...reducedState }
   // only rehydrate if inboundState exists and is an object
   if (inboundState && typeof inboundState === 'object') {
