f1d6f63d04bbb432e917b7a55b85306ff06f00db
Zack Story <zack@root-two.com>
2018-02-02 16:48:45 -0800

    (ops): add umd builds, libsize estimator (#701)
    * (ops): add umd builds, libsize estimator
    
    * cleanup

---
+++ LIBSIZE.md
@@ -0,0 +1,3 @@
+### Redux Persist Size Estimate
+The following is a history of size estimates in bytes. This is calculated as a rollup minified production build, excluding the size of redux which is an assumed peer dependency. YMMV.
+
---
+++ rollup.config.js
@@ -0,0 +1,43 @@
+// @noflow
+import nodeResolve from 'rollup-plugin-node-resolve';
+import babel from 'rollup-plugin-babel';
+import replace from 'rollup-plugin-replace';
+import uglify from 'rollup-plugin-uglify';
+
+const env = process.env.NODE_ENV
+const config = {
+  output: {
+    format: 'umd',
+    name: 'ReduxPersist',
+    exports: 'named',
+    sourcemap: true,  
+  },
+  plugins: [
+    nodeResolve({
+      jsnext: true
+    }),
+    babel({
+      exclude: 'node_modules/**',
+      plugins: ['external-helpers']
+    }),
+    replace({
+      'process.env.NODE_ENV': JSON.stringify(env)
+    })
+  ]
+}
+
+if (env === 'production') {
+  config.plugins.push(
+    uglify({
+      compress: {
+        pure_getters: true,
+        unsafe: true,
+        unsafe_comps: true,
+        warnings: false,
+      },
+    })
+  )
+}
+
+export default config
+
