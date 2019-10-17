6be83aaa64c27e3552bb4b6e473283af07924395
DavidRosenbloom <darosenbloom@gmail.com>
2019-10-17 11:47:38 -0400

    https://github.com/rt2zz/redux-persist/issues/1107 , It seems that when persisting after the initial persist it checks that a persist object exists and not also if the rehydrated property is true, this fixes the issue for me and I havent encountered any breaks

--- src/persistReducer.js
+++ src/persistReducer.js
@@ -106,7 +106,7 @@ export default function persistReducer<State: Object, Action: Object>(
       if (!_persistoid) _persistoid = createPersistoid(config)
 
       // @NOTE PERSIST can be called multiple times, noop after the first
-      if (_persist) {
+      if (_persist && !_persist.rehydrated) {
         // We still need to call the base reducer because there might be nested
         // uses of persistReducer which need to be aware of the PERSIST action
         return {
