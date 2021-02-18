f567c21500708d2af5caa501789c8aabad98d20e
geonwoo.jeong <doscmDev@gmail.com>
2021-02-18 06:55:49 +0000

    fix: type declare persistStore

--- types/persistStore.d.ts
+++ types/persistStore.d.ts
@@ -1,6 +1,6 @@
-declare module "redux-persist/es/persistStore" {
-  import { Store } from 'redux';
-  import { PersistorOptions, Persistor } from "redux-persist/es/types";
+declare module 'redux-persist/es/persistStore' {
+  import { Store, Action, AnyAction } from 'redux';
+  import { PersistorOptions, Persistor } from 'redux-persist/es/types';
 
   /**
    * @desc Creates a persistor for a given store.
@@ -9,10 +9,14 @@ declare module "redux-persist/es/persistStore" {
    * @param callback bootstrap callback of sort.
    */
   // tslint:disable-next-line: strict-export-declare-modifiers
-  export default function persistStore(store: Store, persistorOptions?: PersistorOptions | null, callback?: () => any): Persistor;
+  export default function persistStore<S = any, A extends Action<any> = AnyAction>(
+    store: Store<S, A>,
+    persistorOptions?: PersistorOptions | null,
+    callback?: () => any,
+  ): Persistor;
 }
 
-declare module "redux-persist/lib/persistStore" {
-  export * from "redux-persist/es/persistStore";
-  export { default } from "redux-persist/es/persistStore";
+declare module 'redux-persist/lib/persistStore' {
+  export * from 'redux-persist/es/persistStore';
+  export { default } from 'redux-persist/es/persistStore';
 }
