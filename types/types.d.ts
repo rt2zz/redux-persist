086c8405dce5b9f22110d7fded0f40c5ab07c1d8
Serhiy <szheliznjak@questrade.com>
2021-03-04 17:50:13 +0200

    improved type for white/black lists of persistConfig

--- types/types.d.ts
+++ types/types.d.ts
@@ -31,8 +31,8 @@ declare module "redux-persist/es/types" {
      * @deprecated keyPrefix is going to be removed in v6.
      */
     keyPrefix?: string;
-    blacklist?: Array<string>;
-    whitelist?: Array<string>;
+    blacklist?: Array<keyof S>;
+    whitelist?: Array<keyof S>;
     transforms?: Array<Transform<HSS, ESS, S, RS>>;
     throttle?: number;
     migrate?: PersistMigrate;
