8eb212794692414cb08361061f7c4f45e4e76314
Taro Matsuzawa <btm@tech.email.ne.jp>
2021-08-07 23:45:44 +0900

    remove babel

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,4 @@
 import nodeResolve from 'rollup-plugin-node-resolve';
-import babel from 'rollup-plugin-babel';
 import replace from 'rollup-plugin-replace';
 
 const env = process.env.NODE_ENV
@@ -13,9 +12,6 @@ const config = {
   plugins: [
     nodeResolve({
       jsnext: true
-    }),
-    babel({
-      exclude: 'node_modules/**',
     }),
     replace({
       'process.env.NODE_ENV': JSON.stringify(env)
