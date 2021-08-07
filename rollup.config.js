9bf525b357555e3f5c6bbdbbc572b5a0831ae77e
Taro Matsuzawa <btm@tech.email.ne.jp>
2021-08-07 11:22:55 +0900

    upgrade all packages, remove old configuration, disable uglify

--- rollup.config.js
+++ rollup.config.js
@@ -2,7 +2,6 @@
 import nodeResolve from 'rollup-plugin-node-resolve';
 import babel from 'rollup-plugin-babel';
 import replace from 'rollup-plugin-replace';
-import uglify from 'rollup-plugin-uglify';
 
 const env = process.env.NODE_ENV
 const config = {
@@ -26,16 +25,6 @@ const config = {
 }
 
 if (env === 'production') {
-  config.plugins.push(
-    uglify({
-      compress: {
-        pure_getters: true,
-        unsafe: true,
-        unsafe_comps: true,
-        warnings: false,
-      },
-    })
-  )
 }
 
 export default config
